cef878c6a4cf2e58146f1bd165f2d23959be4489
Rahul Kumar Satiyar <rahul.satiyar.ug20@nsut.ac.in>
2023-09-03 14:32:17 +0530

    5 commit

--- model/Brand.js
+++ model/Brand.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-
+const brandSchema = new Schema({
+  label: { type: String, required: true, unique: true },
+  value: { type: String, required: true, unique: true },
+});
 
 const virtual = brandSchema.virtual('id');
 virtual.get(function () {
